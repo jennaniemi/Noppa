2ab9ff70d4b9a254b03daef2e5f49be9acdbab1f
jennaniemi <jenna.niemi04@gmail.com>
2024-01-30 15:09:36 +0200

    Noppa-sivun tekeminen

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+
+const diceDiv = document.getElementById('dice');
+
+diceDiv.addEventListener('click', () => {
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+    
+    const imagePath = 'img/dice' + randomNumber + '.png';
+    
+    const diceImage = document.querySelector('#dice img');
+    
+    diceImage.src = imagePath;
+});
